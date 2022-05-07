7424de948d17748bc9b8d3f7a1d1ccae2da33e00
KarthikShetty11 <kvshetty27@gmail.com>
2022-05-07 10:38:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
+function App(){
+  return <Header/> 
+}
+
+
+
 
 //FUNCTIONAL COMPONENET
 /*
