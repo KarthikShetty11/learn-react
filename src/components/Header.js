0fe65ae93136bc804be3a43441bd2a2f9987a813
KarthikShetty11 <kvshetty27@gmail.com>
2022-05-07 13:21:21 +0530

    functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 
+/*
 function Header(){
     return <h1>This is header component</h1>
 }
-
 export default Header;
+*/
+
+const Header = ()=>{
+    return <h1>Namaste Header</h1>
+}
+export default Header;
+
+
